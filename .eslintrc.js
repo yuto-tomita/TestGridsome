6f12de7ec7101caf37c1a02eefee2bcab04e4bdf
yuto-tomita <qualidea01@gmail.com>
2020-12-01 09:40:27 +0900

    eslintを導入

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'vue',
+    'gridsome',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+    'plugin:gridsome/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/vue',
+  ],
+  rules: {
+    'prettier/prettier': ['error',
+      {
+        printWidth: 80, // sample
+      }
+    ],
+    'quotes': ['error', 'backtick'], // sample
+  },
+}
