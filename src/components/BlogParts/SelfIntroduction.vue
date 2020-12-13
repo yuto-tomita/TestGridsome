a23023459b45f7e12ff523635ec30930db46d056
yuto-tomita <qualidea01@gmail.com>
2020-12-13 21:56:03 +0900

    自己紹介ページを拡充

--- src/components/BlogParts/SelfIntroduction.vue
+++ src/components/BlogParts/SelfIntroduction.vue
@@ -1,10 +1,28 @@
 <template>
   <!-- <v-card> -->
-  <div>
-    <v-avatar size="62">
-      <g-image src="../../assets/images/self.jpeg"> </g-image>
-    </v-avatar>
-
+  <div class="mt-3">
+    <div style="text-align: center">
+      <v-avatar size="62">
+        <g-image src="../../assets/images/self.jpeg"></g-image>
+      </v-avatar>
+      <h4>ゆうと</h4>
+    </div>
+    <div class="d-flex justify-center">
+      <g-link to="https://twitter.com/qualidea04">
+        <v-fa
+          class="mt-3 sns-icon"
+          color="#1DA1F2"
+          :icon="['fab', 'twitter']"
+        ></v-fa>
+      </g-link>
+      <g-link to="https://github.com/yuto-tomita">
+        <v-fa
+          class="ml-7 mt-3 sns-icon"
+          color="#211F1F"
+          :icon="['fab', 'github']"
+        ></v-fa>
+      </g-link>
+    </div>
     <v-card-text>
       21歳のフロントエンドエンジニアです。 現在お仕事でVue.jsを書いています。
       仕事やプライベートで得た知見をブログを通じ発信していくつもりです！<br />
@@ -17,3 +35,9 @@
 <script lang="ts">
 export default {};
 </script>
+
+<style scoped>
+.sns-icon:hover {
+  opacity: 0.8;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import colors from 'vuetify/es5/util/colors'
 import 'prismjs/themes/prism-tomorrow.css'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFolder, faTags } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add( faFolder, faTags, faClock, faTwitter, faGithub);
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   // Set default layout as a global component
@@ -13,6 +21,7 @@ export default function (Vue, { router, head, isClient, appOptions }) {
     href: 'https://fonts.googleapis.com/css?family=Stylish&display=swap'
   })
   Vue.component('Layout', DefaultLayout)
+  Vue.component("v-fa", FontAwesomeIcon)
   Vue.use(VueCompositionApi)
   Vue.use(Vuetify);
   appOptions.vuetify = new Vuetify({
@@ -28,7 +37,13 @@ export default function (Vue, { router, head, isClient, appOptions }) {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
+        },
+        light: {
+          github: '#211F1F',
+          facebook: '#3B5998',
+          twitter: '#1DA1F2',
+          qiita: '#4cb10d'
+        },
       }
     }
   })
